3245da90a27cacb47d31288b6751bf5960e474fa
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-02-11 21:46:19 -0500

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,31 +1,49 @@
 import WebSocket from 'ws';
-import token from './auth.js'
+import token from './auth.js';
 
-function CLIENT_START() {
+const url = 'wss://gateway.discord.gg/?v=8&encoding=json'
+const socket = new WebSocket(url);
 
-    try {
-        const url = 'wss://gateway.discord.gg/?v=6&encoding=json'
-        let socket = new WebSocket(url);
+function CLIENT_START(call = "s", wait = "r") {
+    try {       
         socket.onmessage = function (event) {
-            const payload = JSON.parse(event.data.toString());
-            const { t, s, op, d } = payload;
-            const p = { op: 1, d: null };
-            switch (op) {
-                case 10:
-                    setInterval(() => {
-                        console.log(`CLIENT: Heartbeat initiated in intervals of 41250`);
-                        socket.send(JSON.stringify(p));
-                    }, 41250);
-                    const properties = { $os: 'linux', $browser: 'behemothlib', $device: 'iOS' };
-                    const intents = { 'intents': 513 };
-                    const identifyinfo = {
-                        op: 2,
-                        d: { token, intents, properties }
-                    };
-                    socket.send(JSON.stringify(identifyinfo));
-                    console.log(event);
-                    break;
-            }
+                    const payload = JSON.parse(event.data.toString());
+                    const { t, s, op, d } = payload;
+                    const p = { op: 1, d: null };
+                    switch (op) {
+                        case 10:
+                                setInterval(() => {
+                                    console.log(`CLIENT: Heartbeat sent`);
+                                    socket.send(JSON.stringify(p));
+                                }, 41250);                           
+                            const identifyinfo = { "op": 2, "d":{ "token": token, "intents": 513, "properties":{ "$os": "linux", "$browser": "behemothlib", "$device": "behemothlib"}} }
+                            socket.send(JSON.stringify(identifyinfo));
+                            console.log("CLIENT: Identified with client");
+                            break;
+                        case 9:
+                            console.log("An error occurred with the connection");
+                            break;
+                        case 0:
+                            switch (t) {
+                                case "MESSAGE_CREATE":
+                                    const content = payload.d.content.toString();
+                                    const isbot = payload.d.content.toString();
+                                    if (content.startsWith("-/") && isbot === "false") {
+                                        const args = content.split("-/")[1].split(" ");
+                                        const command = args[0];
+                                    }
+
+                                    break;
+                            }
+                            break;
+                        case 7:
+                            console.log("RECONNECT ORDERED");
+                            socket.close();
+                            break;
+                        case 1:
+                            console.log("HEARTBEAT ORDERED");
+                            break;
+                    }
         };
     } catch (error) {
         console.log(error);
