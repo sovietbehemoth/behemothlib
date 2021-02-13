4b96b154512fcde12c03048c9d7a8e6a91f2f9b1
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-02-12 23:39:01 -0500

    Create TEMPLATE.js

---
+++ TEMPLATE.js
@@ -0,0 +1,12 @@
+import CLIENT_START from "./websocket.js";
+import message_send from "./functions/messagecreate.js"
+
+CLIENT_START();
+function main(command) {
+    switch (command) {
+        case "ping":
+            message_send("768501915104968755", "Hello");
+            break;
+    }
+}
+export { main }
