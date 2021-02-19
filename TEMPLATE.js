a2088116dd439dbb838cc30fbae0aa334cbabfc2
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-02-19 16:39:37 -0500

    Update TEMPLATE.js

--- TEMPLATE.js
+++ TEMPLATE.js
@@ -2,7 +2,7 @@ import CLIENT_START from "./websocket.js";
 import message_send from "./functions/messagecreate.js"
 
 CLIENT_START();
-function main(command) {
+function main(command, args) {
     switch (command) {
         case "ping":
             message_send("768501915104968755", "Hello");
